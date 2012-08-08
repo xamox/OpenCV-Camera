9e64794dffb875149e7578ed6039d5a17fcdc7c6
Anthony Oliver <email@xamox.net>
2012-08-08 14:46:25 -0400

    Added VideoCapture class example file

---
+++ vidcap.cpp
@@ -0,0 +1,29 @@
+#include "cv.h"
+#include "highgui.h"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**)
+{
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        cout << "Couldn't Open Camera\n";
+        return -1;
+
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
